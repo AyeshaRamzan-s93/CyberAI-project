3d507375c68b5f07f30326b3dce36abd6fbeaa8a
Ayesha Ramzan <ayesha.ramzan@studio93.io>
2025-08-22 01:14:28 -0700

    fix extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 
 
 
-import Navbar from './CyberAI/Navbar/Navbar'
+import Navbar from './CyberAI/Navbar/Navbar.jsx'
 
-import HomeSection from './CyberAI/HomeSection/HomeSection'
+import HomeSection from './CyberAI/HomeSection/HomeSection.jsx'
 
-import Features from './CyberAI/Features/Features'
+import Features from './CyberAI/Features/Features.jsx'
 
-import CyberBox from './CyberAI/CyberBox/CyberBox'
+import CyberBox from './CyberAI/CyberBox/CyberBox.jsx'
 
-import CyberUniverse from './CyberAI/CyberUniverse/CyberUniverse'
+import CyberUniverse from './CyberAI/CyberUniverse/CyberUniverse.jsx'
 
-import PortalSection from './CyberAI/Portal/PortalSection'
+import PortalSection from './CyberAI/Portal/PortalSection.jsx'
 
-import FAQ from './CyberAI/FAQ/FAQ'
+import FAQ from './CyberAI/FAQ/FAQ.jsx'
 
-import Footer from './CyberAI/Footer/Footer'
+import Footer from './CyberAI/Footer/Footer.jsx'
 
 function App() {
   return (
