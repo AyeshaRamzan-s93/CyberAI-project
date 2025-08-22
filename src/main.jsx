2f6d48c22e0023c26a8a4c9810cdfd3712088e2a
Ayesha Ramzan <ayesha.ramzan@studio93.io>
2025-08-21 20:18:44 -0700

    git commit -m "Add explicit .jsx extensions for Vercel build"

--- src/main.jsx
+++ src/main.jsx
@@ -5,22 +5,22 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import ActivityApp from "./Mock Website/MockApp.jsx";
 // import App from './App.jsx'
-import Navbar from './CyberAI/Navbar/Navbar'
+import Navbar from './CyberAI/Navbar/Navbar.jsx'
 
-import HomeSection from './CyberAI/HomeSection/HomeSection'
+import HomeSection from './CyberAI/HomeSection/HomeSection.jsx'
 
 
-import Features from './CyberAI/Features/Features'
+import Features from './CyberAI/Features/Features.jsx'
 
-import CyberBox from './CyberAI/CyberBox/CyberBox'
+import CyberBox from './CyberAI/CyberBox/CyberBox.jsx'
 
-import CyberUniverse from './CyberAI/CyberUniverse/CyberUniverse'
+import CyberUniverse from './CyberAI/CyberUniverse/CyberUniverse.jsx'
 
-import PortalSection from './CyberAI/Portal/PortalSection'
+import PortalSection from './CyberAI/Portal/PortalSection.jsx'
 
-import FAQ from './CyberAI/FAQ/FAQ'
+import FAQ from './CyberAI/FAQ/FAQ.jsx'
 
-import Footer from './CyberAI/Footer/Footer'
+import Footer from './CyberAI/Footer/Footer.jsx'
 
 
 
